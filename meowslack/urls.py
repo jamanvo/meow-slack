4e3bfd7f43e64b6855717e8e73346099d0ee8fd4
Hyunkyung Ahn <tuwhit1202@gmail.com>
2019-02-18 20:55:42 +0900

    OAuth url 추가

--- meowslack/urls.py
+++ meowslack/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from slackbot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'auth?$', views.auth.as_view(), name='slack_auth')
 ]
